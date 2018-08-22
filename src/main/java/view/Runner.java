658293ed3bfc23bfcd00e17aab3bb387adc76f55
eatrzcinska <eatrzcinska@gmail.com>
2018-08-22 22:41:28 +0200

    Added menu

--- src/main/java/view/Runner.java
+++ src/main/java/view/Runner.java
@@ -19,16 +19,16 @@ public class Runner {
 
     public void run() {
         System.out.println("Welcome to PASSWORD KEEPER!");
-        while(true){
+        while(state!=FINISH_PROGRAM){
             askUser();
         }
-
+        System.out.println("Goodbye!");
     }
 
     public void askUser() {
         if (state == START_MENU) {
             System.out.println("What would you like to do?");
-            System.out.println("1. Add password, 2. Show password, 3. Finish program");
+            System.out.println("1. Add password, 2. Show all passwords, 3. Remove password, 4. Finish program");
             int answer = Integer.parseInt(scanner.nextLine());
             if (answer == 1) {
                 state = ADD_PASSWORD;
@@ -39,19 +39,23 @@ public class Runner {
                 return;
             }
             if (answer == 3) {
+                state = REMOVE_PASSWORD;
+                return;
+            }
+            if (answer == 4) {
                 state = FINISH_PROGRAM;
                 return;
             }
         }
 
         if (state == ADD_PASSWORD) {
-            System.out.println("You chose 1. Add password");
+            System.out.println("You have chosen 1. Add password");
             System.out.println("Give website");
             String website = scanner.nextLine();
             System.out.println("Give login");
             String login = scanner.nextLine();
             System.out.println("Give number of letters");
-            int number = Integer.parseInt(scanner.next());
+            int number = Integer.parseInt(scanner.nextLine());
             System.out.println("Give type of generator");
             System.out.println("1. Lower and upper case, numbers, special characters");
             System.out.println("2. Lower and upper case, numbers");
@@ -60,32 +64,44 @@ public class Runner {
             GeneratorType generator=null;
             if (answer == 1) {
                 generator = GeneratorType.TYPE1;
-                return;
-            }
-            if (answer == 2) {
-                generator = GeneratorType.TYPE1;
-                return;
-            }
-            if (answer == 3) {
-                generator = GeneratorType.TYPE1;
-                return;
+            } else if (answer == 2) {
+                generator = GeneratorType.TYPE2;
+            } else if (answer == 3) {
+                generator = GeneratorType.TYPE3;
             }
             FilesFacade filesFacade = new FilesFacade();
-            List<String> listOfStrings = filesFacade.readFile("PasswordKeeper.txt");
+            //List<String> listOfStrings = filesFacade.readFile("PasswordKeeper.txt");
             List<PasswordEntry> listOfObjects = filesFacade.getEntries("PasswordKeeper.txt");
-            List<PasswordEntry> listaRekordow = new ArrayList<>();
+            List<PasswordEntry> listaRekordow = new ArrayList<>(listOfObjects);
             PasswordFacade ostateczny = new PasswordFacade();
             PasswordEntry rekord = ostateczny.generatePasswordEntry(
                     website,login,generator,number);
             listaRekordow.add(rekord);
             filesFacade.writeToFile("PasswordKeeper.txt",listaRekordow);
+            System.out.println("You have just added new password");
             state = START_MENU;
+            return;
         }
         if (state == SHOW_PASSWORD) {
-
+            System.out.println("You have chosen 2. Show all passwords");
+            FilesFacade filesFacade = new FilesFacade();
+            List<PasswordEntry> listOfObjects = filesFacade.getEntries("PasswordKeeper.txt");
+            for(PasswordEntry line: listOfObjects){
+                System.out.println(listOfObjects.indexOf(line)+". " + line);
+            }
+            state = START_MENU;
+            return;
         }
-        if (state == FINISH_PROGRAM) {
-            System.out.println("Goodbye");
+        if (state == REMOVE_PASSWORD) {
+            System.out.println("Which password would you like to remove? (Give a number)");
+            int index = Integer.parseInt(scanner.nextLine());
+            FilesFacade filesFacade = new FilesFacade();
+            List<PasswordEntry> listOfObjects = filesFacade.getEntries("PasswordKeeper.txt");
+            List<PasswordEntry> listaRekordow = new ArrayList<>(listOfObjects);
+            listaRekordow.remove(index);
+            filesFacade.writeToFile("PasswordKeeper.txt",listaRekordow);
+            System.out.println("You have just removed a password");
+            state = START_MENU;
             return;
         }
 
